a40fac7604437e7a3f086541d73c633b409682e0
Gatrrr <davidgatrmacaulay@gmail.com>
2019-09-09 13:33:54 -0700

    MVP finished

--- src/BottomRow.js
+++ src/BottomRow.js
@@ -1,5 +1,6 @@
-import React from "react";
+import React, { useState } from "react";
 import "./App.css";
+
 
 const BottomRow = () => {
   return (
@@ -10,15 +11,15 @@ const BottomRow = () => {
       </div>
       <div className="toGo">
         <h3 className="toGo__title">To Go</h3>
-        <div className="toGo__value">7</div>
+        <div className="toGo__value">8</div>
       </div>
       <div className="ballOn">
         <h3 className="ballOn__title">Ball on</h3>
-        <div className="ballOn__value">21</div>
+        <div className="ballOn__value">8</div>
       </div>
       <div className="quarter">
         <h3 className="quarter__title">Quarter</h3>
-        <div className="quarter__value">4</div>
+        <div className="quarter__value">3</div>
       </div>
     </div>
   );
